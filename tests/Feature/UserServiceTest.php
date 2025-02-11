4a65bc3aa42896cb0f898e9c511b900f225678f3
TiaWidi <tia.septianawidi@gmail.com>
2025-02-11 22:15:38 +0700

    Initial UserService

---
+++ tests/Feature/UserServiceTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Services\UserService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserServiceTest extends TestCase
+{
+    private UserService $userService;
+
+    /**
+     * A basic feature test example.
+     */
+    // public function test_example(): void
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->userService = $this->app->make(UserService::class);
+    }
+
+    public function testSample()
+    {
+        self::assertTrue(true);
+    }
+}
